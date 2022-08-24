32101cdf43109f8fc41b46c96d4f45bfc30a1319
Ignacio Baptista <ibaptista@grupomercel.com>
2022-08-24 00:18:58 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tp-tacs-2022-2c-grupo-2-frontend';
+}
