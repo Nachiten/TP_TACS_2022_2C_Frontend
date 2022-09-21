0f203e51164728d79bb8e63ab3b1e43847f6246e
peykov <peykovn@gmail.com>
2022-09-21 01:04:18 -0300

    add base logic

--- src/app/core/components/match-details-form/match-details-form.component.ts
+++ src/app/core/components/match-details-form/match-details-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { controlHasError, getControlValidClass } from 'src/app/utils/form-utils';
+
 
 @Component({
   selector: 'app-match-details-form',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./match-details-form.component.css']
 })
 export class MatchDetailsFormComponent implements OnInit {
+  controlHasError = controlHasError;
+  getControlValidClass = getControlValidClass;
+  matchDetails: string = '';
 
-  constructor() { }
+  constructor(
+    private readonly toastr: ToastrService
+  ) { }
+
+  matchDetailsForm = new FormGroup({
+    matchId: new FormControl('', {
+      validators: [Validators.required],
+      nonNullable: true
+    })
+  });
+
+  controls = this.matchDetailsForm.controls;
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    if (!this.matchDetailsForm.valid) {
+      console.log('Invalid form. Canceling submit.');
+
+      this.toastr.error('Debe corregir todos los errores antes de avanzar', 'Error!');
+      this.matchDetailsForm.markAllAsTouched();
+
+      return;
+    }
+
+    const matchID = this.matchDetailsForm.value.matchId as string;
+
+    if(!matchID) {
+      this.toastr.error(`El id de partido es obligatorio`, 'Error!');
+      return;
+    }
+
+    this.matchDetails = matchID
+
+    console.log(matchID)
+
   }
 
 }
