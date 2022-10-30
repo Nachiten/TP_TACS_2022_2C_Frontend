517f5ad656fdf98c607b27809669cdb296e27cd6
Walter Schmidt <wschmidt@frba.utn.edu.ar>
2022-10-29 21:20:52 -0300

    Agrego buildspec

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { getApiResources } from '../app/config/api-resources';
 export const environment = {
   production: true,
   apiResources: getApiResources(),
-  apiURL: 'http://192.168.92.2:3000/api'
+  apiURL: '/api'
 };
